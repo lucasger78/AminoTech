beb006b1d2733674394a9325fc9d07c9075a3797
Lucas Conti <lucasger78@hotmail.com>
2023-12-18 23:41:44 -0300

    actualización 85%

--- js/sweetalert.js
+++ js/sweetalert.js
@@ -1,77 +1,14 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const formulario = document.querySelector('form');
-
-//     formulario.addEventListener('submit', function (event) {
-//         event.preventDefault(); // Evitar que el formulario se envíe normalmente
-
-//         // Lógica de validación del formulario
-//         const nombre = formulario.querySelector('input[placeholder="Nombre"]').value;
-//         const email = formulario.querySelector('input[placeholder="E-mail"]').value;
-//         const asunto = formulario.querySelector('input[placeholder="Asunto"]').value;
-//         const mensaje = formulario.querySelector('textarea').value;
-
-//         if (!nombre || !email || !asunto || !mensaje) {
-//             // Si algún campo está vacío, muestra un mensaje de error
-//             Swal.fire({
-//                 title: 'Error',
-//                 text: 'Por favor, completa todos los campos del formulario.',
-//                 icon: 'error',
-//                 confirmButtonText: 'OK'
-//             });
-//         } else {
-//             // Envía el formulario utilizando la API Fetch
-//             fetch(formulario.action, {
-//                 method: formulario.method,
-//                 body: new FormData(formulario)
-//             })
-//             .then(response => {
-//                 if (response.ok) {
-//                     // Si el formulario se envía con éxito, reinicia el formulario y luego muestra el SweetAlert
-//                     formulario.reset();
-//                     Swal.fire({
-//                         title: '¡Enviado!',
-//                         text: 'Gracias por tu mensaje.',                                                
-//                         imageUrl: "logo2.png",
-//                         icon: 'success',
-//                         confirmButtonText: 'OK',
-//                         confirmButtonColor: '#3a3a3a',
-//                         customClass: {                            
-//                             content: 'swal2-text',
-//                             confirmButton: 'swal2-text2'  
-//                         }
-//                     })
-//                     .then(() => {
-//                         // Después de hacer clic en OK, recarga la página
-//                         window.location.reload();
-//                     });
-//                 } else {
-//                     // Si hay un error en el envío, muestra un mensaje de error
-//                     Swal.fire({
-//                         title: 'Error',
-//                         text: 'Hubo un problema al enviar el formulario. Inténtalo de nuevo más tarde.',
-//                         icon: 'error',
-//                         confirmButtonText: 'OK'
-//                     });
-//                 }
-//             })
-//             .catch(error => {
-//                 console.error('Error en la solicitud fetch:', error);
-//             });
-//         }
-//     });
-// });
 
 document.addEventListener('DOMContentLoaded', function () {
-    const formulario = document.querySelector('form');
+    const myForm = document.getElementById('myForm');
 
-    formulario.addEventListener('submit', function (event) {
+    myForm.addEventListener('submit', function (event) {
         event.preventDefault(); // Evitar que el formulario se envíe normalmente
 
-        // Lógica de validación del formulario
-        const nombre = formulario.querySelector('input[placeholder="Nombre"]').value;
-        const email = formulario.querySelector('input[placeholder="E-mail"]').value;
-        const asunto = formulario.querySelector('input[placeholder="Asunto"]').value;
-        const mensaje = formulario.querySelector('textarea').value;
+        const nombre = myForm.querySelector('[name="nombre"]').value;
+        const email = myForm.querySelector('[name="email"]').value;
+        const asunto = myForm.querySelector('[name="asunto"]').value;
+        const mensaje = myForm.querySelector('[name="mensaje"]').value;
 
         if (!nombre || !email || !asunto || !mensaje) {
             // Si algún campo está vacío, muestra un mensaje de error
@@ -79,92 +16,27 @@ document.addEventListener('DOMContentLoaded', function () {
                 title: 'Error',
                 text: 'Por favor, completa todos los campos del formulario.',
                 icon: 'error',
-                confirmButtonText: 'OK'
+                confirmButtonText: 'OK',
+                customClass: {
+                    confirmButton: 'swal-confirm-button-error'
+                }
             });
         } else {
-            // Envía el formulario utilizando la API Fetch
-            fetch(formulario.action, {
-                method: formulario.method,
-                body: new FormData(formulario)
-            })
-            .then(response => {
-                if (response.ok) {
-                    // Si el formulario se envía con éxito, reinicia el formulario y luego muestra el SweetAlert
-                    formulario.reset();
-                    Swal.fire({
-                        title: '¡Enviado!',
-                        text: 'Gracias por tu mensaje.',
-                        icon: 'success',
-                        imageUrl: "img/logo-head.png",  
-                        confirmButtonText: 'OK',
-                        confirmButtonColor: '#3a3a3a',
-                        customClass: {                            
-                            content: 'swal2-text',
-                            confirmButton: 'swal2-text2'  
-                        }
-                    })
-                    .then(() => {
-                        // Después de hacer clic en OK, puedes decidir si recargar la página o hacer algo más
-                    });
-                } else {
-                    // Si hay un error en el envío, muestra un mensaje de error
-                    Swal.fire({
-                        title: 'Error',
-                        text: 'Hubo un problema al enviar el formulario. Inténtalo de nuevo más tarde.',
-                        icon: 'error',
-                        confirmButtonText: 'OK'
-                    });
+            // Si todos los campos están llenos, muestra un mensaje de éxito
+            Swal.fire({
+                imageUrl: "img/logo-head.png",
+                title: '¡Enviado!',
+                text: 'Gracias por tu mensaje.',
+                icon: 'success',
+                confirmButtonText: 'OK',
+                customClass: {
+                    title: 'swal-title-success',
+                    text:  'swal-text-success'
                 }
-            })
-            .catch(error => {
-                console.error('Error en la solicitud fetch:', error);
+            }).then(() => {
+                // Después de hacer clic en OK, reinicia el formulario
+                myForm.reset();
             });
         }
     });
 });
-
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     const form = document.getElementById('myForm');
-
-//     form.addEventListener('submit', function (event) {
-//         event.preventDefault();
-
-        
-//         if (!validateForm()) {
-//             return;
-//         }
-
-       
-//         Swal.fire({
-//             title: 'Enviado',
-//             text: '¡Gracias por contactarnos!',
-//             imageUrl: "img/logo-head.png",
-//             icon: 'success',
-//             confirmButtonText: 'OK'
-//         }).then(() => {
-            
-//             form.reset();
-//         });
-//     });
-
-//     function validateForm() {
-//         const inputs = form.querySelectorAll('input, textarea');
-
-//         for (const input of inputs) {
-//             if (!input.value.trim()) {
-                
-//                 Swal.fire({
-//                     title: 'Error',
-//                     text: 'Por favor, completa todos los campos del formulario.',
-//                     icon: 'error',
-//                     confirmButtonText: 'OK'
-//                 });
-//                 return false; 
-//             }
-//         }
-
-//         return true; 
-//     }
-// });
-
